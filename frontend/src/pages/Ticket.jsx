be71262bef186517ef0fdbb817e1eaf963104a93
unknown <ugurcan.uut82@gmail.com>
2022-10-17 23:25:36 +0300

    ticket functionalty

---
+++ frontend/src/pages/Ticket.jsx
@@ -0,0 +1,71 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import { getTicket, reset, closeTicket } from "../features/tickets/ticketSlice";
+
+function Ticket() {
+  const { isLoading, isSuccess, isError, message, ticket } = useSelector(
+    (state) => state.tickets
+  );
+
+  const dispatch = useDispatch();
+
+  const params = useParams();
+  const navigate = useNavigate();
+  const { ticketId } = useParams();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    dispatch(getTicket(ticketId));
+    // eslint-disable-next-line
+  }, [isError, ticketId, message]);
+
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId));
+    toast.success("Ticket Closed");
+    navigate("/tickets");
+  };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h3>Sometign Went Wrong</h3>;
+  }
+
+  return (
+    <div className="ticket-page">
+      <header className="ticket-header">
+        <BackButton url="/tickets" />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Data Submitted: {new Date(ticket.createdAt).toLocaleString("tr-TR")}
+        </h3>
+        <h3>Product: {ticket.product}</h3>
+        <hr />
+        <div className="ticket-desc">
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </header>
+      {ticket.status !== "closed" && (
+        <button onClick={onTicketClose} className="btn btn-block btn-danger">
+          Close Ticket
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Ticket;
